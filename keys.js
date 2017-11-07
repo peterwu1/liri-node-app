2d270b81662f847b23930fcd0c8ebcbae8faabbd
Peter <peter@Peters-MacBook-Air.local>
2017-11-06 22:58:23 -0800

    Initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<f4yCN548jo09Gy5MgfoSINumc>',
+  consumer_secret: '<AbXNeY9RSk7VobvjoUg9OlDUtlFNulusy0AD0jD5HB54OzkOZ5>',
+  access_token_key: '<927776713999659008-QQrU0BPG1D9CaNK2QtGEblMs4Y08TJl>',
+  access_token_secret: '<EM9UdzhGo2fFFoOS89DonCTVyAsI93PjQK4Y9PrlZoOFb>',
+}
+
+module.exports = twitterKeys;
